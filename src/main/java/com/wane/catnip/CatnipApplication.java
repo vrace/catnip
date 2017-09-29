55590ef21402b41e0b2cbf592dc375dcafa33596
Yang <vrace_studios@hotmail.com>
2017-09-29 10:00:01 +0800

    initial commit

---
+++ src/main/java/com/wane/catnip/CatnipApplication.java
@@ -0,0 +1,12 @@
+package com.wane.catnip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatnipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatnipApplication.class, args);
+	}
+}
