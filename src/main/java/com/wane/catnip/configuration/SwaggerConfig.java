d658cbc7cf4b9e1fd4dcc0d5dfd0a7dd8f630c69
Yang <vrace_studios@hotmail.com>
2017-10-12 15:48:12 +0800

    change to use swagger 2

--- src/main/java/com/wane/catnip/configuration/SwaggerConfig.java
+++ src/main/java/com/wane/catnip/configuration/SwaggerConfig.java
@@ -1,23 +1,32 @@
 package com.wane.catnip.configuration;
 
-import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
-import com.mangofactory.swagger.models.dto.ApiInfo;
-import com.mangofactory.swagger.plugin.EnableSwagger;
-import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger
+@EnableSwagger2
 public class SwaggerConfig {
-    @Autowired
-    private SpringSwaggerConfig springSwaggerConfig;
-
     @Bean
-    public SwaggerSpringMvcPlugin catnipApiConfiguration() {
-        return new SwaggerSpringMvcPlugin(springSwaggerConfig)
-                .apiInfo(new ApiInfo("CATNIP API", "", "", "", "", ""))
-                .includePatterns("/appearance/.*", "/moment/.*");
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.wane.catnip.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("CATNIP API")
+                .version("0.1")
+                .build();
     }
 }
--- src/main/java/com/wane/catnip/controller/AppearanceController.java
+++ src/main/java/com/wane/catnip/controller/AppearanceController.java
@@ -2,8 +2,8 @@ package com.wane.catnip.controller;
 
 import com.wane.catnip.model.Appearance;
 import com.wane.catnip.service.appearance.AppearanceService;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
